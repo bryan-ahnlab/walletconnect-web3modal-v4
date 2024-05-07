96addbc791ffcf97d7336edee838e5cf249adb51
bryan-ahnlab <bryan@ahnlab.io>
2024-05-07 20:18:26 +0900

    feat: add webpack based react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="">webpack-react-app-sample</div>
+    </>
+  );
+};
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const InterpolateHtmlPlugin = require("interpolate-html-plugin");
+const production = (process.env.NODE_ENV = "production");
+
+module.exports = {
+  mode: production ? "production" : "development",
+  devtool: production ? "hidden-source-map" : "eval",
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+    static: path.resolve(__dirname, "dist"),
+    /* proxy: "localhost:3000", */
+  },
+
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new InterpolateHtmlPlugin({ PUBLIC_URL: "" }),
+  ],
+};
