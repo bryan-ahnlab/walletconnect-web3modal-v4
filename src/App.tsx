eb9ef902845b9b96d86a35bb7f7ce8579496163a
bryan-ahnlab <bryan@ahnlab.io>
2024-05-09 22:02:48 +0900

    fix: add exception handling and verify sign typed data code

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,21 @@ import {
   useSendTransaction,
   useEstimateGas,
 } from "wagmi";
-import { parseEther, type Address } from "viem";
+import {
+  parseEther,
+  type Address,
+  recoverTypedDataAddress,
+  Signature,
+  SignTypedDataReturnType,
+} from "viem";
 
 function App() {
   const account = useAccount();
   const { status, error } = useConnect();
   const { disconnect } = useDisconnect();
-  const { signMessage } = useSignMessage();
-  const { signTypedData } = useSignTypedData();
-  const { sendTransaction } = useSendTransaction();
+  const { signMessageAsync } = useSignMessage();
+  const { signTypedDataAsync } = useSignTypedData();
+  const { sendTransactionAsync } = useSendTransaction();
 
   async function handleConnect() {
     try {
@@ -39,65 +45,93 @@ function App() {
   }
 
   async function handleSignMessage() {
-    const data = "hello world!";
-    signMessage({ message: data });
+    try {
+      const data = "hello world!";
+      const response = await signMessageAsync({ message: data });
+      console.info(`handleSignMessage: ${response}`);
+    } catch {
+      console.error(`handleSignMessage: Failed to sign message`);
+    }
   }
   async function handleSignTypedData() {
-    const types = {
-      Person: [
-        { name: "name", type: "string" },
-        { name: "wallet", type: "address" },
-      ],
-      Mail: [
-        { name: "from", type: "Person" },
-        { name: "to", type: "Person" },
-        { name: "contents", type: "string" },
-      ],
-    } as const;
-    const message = {
-      from: {
-        name: "Cow",
-        wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
-      },
-      to: {
-        name: "Bob",
-        wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
-      },
-      contents: "Hello, Bob!",
-    } as const;
-    const domain = {
-      name: "Ether Mail",
-      version: "1",
-      chainId: 1001,
-      verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
-    } as const;
-    const data: {
-      domain: typeof domain;
-      message: typeof message;
-      primaryType: "Mail";
-      types: typeof types;
-    } = {
-      domain,
-      message,
-      primaryType: "Mail",
-      types,
-    };
-    signTypedData(data);
+    try {
+      const types = {
+        Person: [
+          { name: "name", type: "string" },
+          { name: "wallet", type: "address" },
+        ],
+        Mail: [
+          { name: "from", type: "Person" },
+          { name: "to", type: "Person" },
+          { name: "contents", type: "string" },
+        ],
+      } as const;
+      const message = {
+        from: {
+          name: "Cow",
+          wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+        },
+        to: {
+          name: "Bob",
+          wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+        },
+        contents: "Hello, Bob!",
+      } as const;
+      const domain = {
+        name: "Ether Mail",
+        version: "1",
+        chainId: 1001,
+        verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+      } as const;
+      const data: {
+        domain: typeof domain;
+        message: typeof message;
+        primaryType: "Mail";
+        types: typeof types;
+      } = {
+        domain,
+        message,
+        primaryType: "Mail",
+        types,
+      };
+      let signature: SignTypedDataReturnType | Signature =
+        await signTypedDataAsync(data);
+      console.info(`handleSignTypedData - signature: ${signature}`);
+      const response = await recoverTypedDataAddress({ ...data, signature });
+      console.info(`handleSignTypedData - signing address: ${account.address}`);
+      console.info(`handleSignTypedData - recovered address: ${response}`);
+      console.info(
+        `handleSignTypedData - verified: ${
+          (account.address as unknown as String) == (response as String)
+        }`
+      );
+    } catch {
+      console.error(`handleSignTypedData: Failed to sign typed data`);
+    }
   }
 
-  const sendTransactionData = {
+  const sendTransactionAsyncData = {
     to: process.env.REACT_APP_TEST_ACCOUNT as Address,
     value: parseEther("0.001"),
   };
-  const { data: gas, error: prepareError } =
-    useEstimateGas(sendTransactionData);
-  const handleSendTransaction = useCallback(() => {
+  const { data: gas, error: prepareError } = useEstimateGas(
+    sendTransactionAsyncData
+  );
+  const handleSendTransaction = useCallback(async () => {
     if (prepareError) {
-      console.error(prepareError);
+      console.error(`handleSendTransaction: ${prepareError}`);
     } else {
-      sendTransaction({ ...sendTransactionData, gas });
+      try {
+        const response = await sendTransactionAsync({
+          ...sendTransactionAsyncData,
+          gas,
+        });
+        console.info(`handleSendTransaction: ${response}`);
+      } catch (error) {
+        console.error(`handleSendTransaction: ${error}`);
+      }
     }
-  }, [sendTransaction, prepareError]);
+  }, [sendTransactionAsync, prepareError]);
 
   return (
     <div className="App" style={{ textAlign: "center", padding: "0 2rem" }}>
