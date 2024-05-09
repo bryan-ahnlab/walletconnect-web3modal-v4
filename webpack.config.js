7795a924f85d4f5c25cea11f96eb954d9016b188
bryan-ahnlab <bryan@ahnlab.io>
2024-05-09 13:11:25 +0900

    feat: add webpack based react app with babel and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,11 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(js|jsx)$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
     ],
   },
   output: {
